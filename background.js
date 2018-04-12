54a10af97c2b146ab239409e51eb8b9a964d6c9b
FutureX <dimal2000futurex2000@gmail.com>
2018-04-13 00:09:16 +0300

    Version 1.1.0
    Добавил контекстное меню, вызываемое правой кнопкой мыши. Теперь там можно нажать кнопку "Посмотреть ответы"
    
    Улучшил вид popup меню, добавил css для красивого оформления
    
    Теперь скрипты показа ответов не будет грузить лишний раз браузер, будет работать только в нужных фреймах

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
-chrome.storage.sync.get(['choise'], function(items) {
+chrome.storage.sync.get(['choise','simple'], function(items) {
 	var choise=items['choise'];
 	if(choise == undefined) choise = true;
 	if(choise) chrome.browserAction.setBadgeText({text: "on"});
 	else chrome.browserAction.setBadgeText({text: "off"});
 });
+
+chrome.contextMenus.create({"title": "Показать ответы","onclick" : show});
+
+function show(info) {
+	var url = info.pageUrl;
+	var url_ = "www.cambridgelms.org";
+	if(JSON.stringify(url).includes(url_))
+	{
+		var query = { active: true, currentWindow: true };
+		chrome.tabs.query(query, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {command: "show"});
+		});
+	}
+}
--- inject.js
+++ inject.js
@@ -1,21 +1,27 @@
 //По окончании загрузки документа вызывается функция
 $(document).ready(function() {
-	chrome.storage.sync.get(['choise'], function(items) {
-		var choise=items['choise'];
-		if(choise == undefined) choise = true;
-		if(choise){
-			var timerId = setInterval(show, 2000);
-			setTimeout(function() {
-				clearInterval(timerId);
-			}, 7000);
-		}
-    });
+	var path = window.location.pathname;
+	var page = path.split("/").pop();
+	if(page === 'index.html') {
+		//При получении команды 'show' показывает ответы
+		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { 
+			if(request.command==="show") {
+				show();
+			}
+		});
+		//Если выбрана функция автоматического показа ответов заппускается функция показа ответов
+		chrome.storage.sync.get(['choise'], function(items) {
+			var choise=items['choise'];
+			if(choise == undefined) choise = true;
+			if(choise){
+				var timerId = setInterval(show, 2000);
+				setTimeout(function() {
+					clearInterval(timerId);
+				}, 7000);
+			}
+		});
+	}
 })
-
-//При получении команды 'show' показывает ответы
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { 
-	if(request.command==="show") show();
-});
 
 //Показать ответы
 function show() { 
